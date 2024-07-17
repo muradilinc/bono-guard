a7e55de9882e87c535016766178f2c102dd59e88
Muradil <muradil.koychubekob@gmail.com>
2024-07-17 14:19:57 +0600

    change password for crm user

--- routes/usersCrm.ts
+++ routes/usersCrm.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-// import {transporter} from "../mailer";
+import {transporter} from "../mailer";
 import UserCrm from "../models/UserCRM";
 
 const usersCrmRouter = express.Router();
@@ -73,73 +73,73 @@ usersCrmRouter.delete('/sessions', async (req, res, next) => {
     }
 });
 
-// usersCrmRouter.post('/send-otp', async (req, res, next) => {
-//     try {
-//         const email = req.body.email;
-//         const user = await User.findOne({ email });
-//
-//         if (!user) {
-//             return res.status(422).send({ error: 'Пользователь не найден' });
-//         }
-//
-//         const otp = Math.floor(Math.random() * 900000).toString();
-//
-//         user.otp = otp;
-//
-//         await user.save();
-//
-//         const mailOptions = {
-//             from: process.env['USER_MAILER'],
-//             to: email,
-//             subject: 'Your OTP Code to Bono',
-//             text: `Ваш OTP код ${otp}`,
-//         };
-//
-//         transporter.sendMail(mailOptions, (error, _info) => {
-//             if (error) {
-//                 return next(error);
-//             } else {
-//                 return res.status(200).send({ message: `OTP отправлен на ${req.body.email}` });
-//             }
-//         });
-//     } catch (e) {
-//         return next(e);
-//     }
-// });
-//
-// usersRouter.post('/compare-otp', async (req, res, next) => {
-//     try {
-//         const user = await User.findOne({ email: req.body.email });
-//         const otp = req.body.otp;
-//
-//         if (user?.otp !== otp) {
-//             return res.status(422).send({ error: 'Неверный Otp' });
-//         }
-//
-//         return res.status(200).send({ message: 'Success' });
-//     } catch (e) {
-//         return next(e);
-//     }
-// });
-//
-// usersRouter.post('/change-password', async (req, res, next) => {
-//     try {
-//         const { email, password } = req.body;
-//         const user = await User.findOne({ email });
-//
-//         if (!user) {
-//             return res.status(422).send({ error: 'Пользователь не найден' });
-//         }
-//
-//         user.password = password;
-//         user.generateToken();
-//
-//         await user.save();
-//
-//         return res.send({ message: 'Пароль изменен' });
-//     } catch (e) {
-//         return next(e);
-//     }
-// });
+usersCrmRouter.post('/send-otp', async (req, res, next) => {
+    try {
+        const email = req.body.email;
+        const user = await UserCrm.findOne({ email });
+
+        if (!user) {
+            return res.status(422).send({ error: 'Пользователь не найден' });
+        }
+
+        const otp = Math.floor(Math.random() * 900000).toString();
+
+        user.otp = otp;
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env['USER_MAILER'],
+            to: email,
+            subject: 'Your OTP Code to Bono',
+            text: `Ваш OTP код ${otp}`,
+        };
+
+        transporter.sendMail(mailOptions, (error, _info) => {
+            if (error) {
+                return next(error);
+            } else {
+                return res.status(200).send({ message: `OTP отправлен на ${req.body.email}` });
+            }
+        });
+    } catch (e) {
+        return next(e);
+    }
+});
+
+usersCrmRouter.post('/compare-otp', async (req, res, next) => {
+    try {
+        const user = await UserCrm.findOne({ email: req.body.email });
+        const otp = req.body.otp;
+
+        if (user?.otp !== otp) {
+            return res.status(422).send({ error: 'Неверный Otp' });
+        }
+
+        return res.status(200).send({ message: 'Success' });
+    } catch (e) {
+        return next(e);
+    }
+});
+
+usersCrmRouter.post('/change-password', async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserCrm.findOne({ email });
+
+        if (!user) {
+            return res.status(422).send({ error: 'Пользователь не найден' });
+        }
+
+        user.password = password;
+        user.generateToken();
+
+        await user.save();
+
+        return res.send({ message: 'Пароль изменен' });
+    } catch (e) {
+        return next(e);
+    }
+});
 
 export default usersCrmRouter;
